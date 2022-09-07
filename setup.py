d6bc354a8b2fa98ff7d5bfff2e33f2c00bac69c0
Gaffer Fitch <gaffer.fitch@gmail.com>
2022-09-07 10:08:42 -0400

    Setup using central version for build

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup
 
 with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
 
-with open("VERSION", "r") as version_file:
-    version = version_file.read().strip()
+version = os.environ.get("BUILD_VERSION")
+
+if version is None:
+    with open("VERSION", "r") as version_file:
+        version = version_file.read().strip()
 
 setup(
     name="sphinxter",
